7616d37398cf0c5a150f83e90667b4e1bde2bd72
xmiy <gohell69@hotmail.com>
2017-08-17 06:53:12 +0800

    Fixed! SliderProgressExample.xaml.cs

--- src/MahApps.Metro.Samples/MahApps.Metro.Demo/MahApps.Metro.Demo.Shared/ExampleViews/SliderProgressExamples.xaml.cs
+++ src/MahApps.Metro.Samples/MahApps.Metro.Demo/MahApps.Metro.Demo.Shared/ExampleViews/SliderProgressExamples.xaml.cs
@@ -18,51 +18,51 @@ namespace MetroDemo.ExampleViews
 
         private void RangeSlider_OnLowerValueChanged(object sender, RangeParameterChangedEventArgs e)
         {
-            MessageBox.Show(e.OldValue.ToString() + "->" + e.NewValue.ToString());
+            //MessageBox.Show(e.OldValue.ToString() + "->" + e.NewValue.ToString());
         }
 
         private void RangeSlider_OnUpperValueChanged(object sender, RangeParameterChangedEventArgs e)
         {
-            MessageBox.Show(e.OldValue.ToString() + "->" + e.NewValue.ToString());
+            //MessageBox.Show(e.OldValue.ToString() + "->" + e.NewValue.ToString());
         }
 
         private void RangeSlider_OnLowerThumbDragStarted(object sender, DragStartedEventArgs e)
         {
-            TestBlock.Text = "lower thumb drag started";
+            //TestBlock.Text = "lower thumb drag started";
         }
 
         private void RangeSlider_OnLowerThumbDragCompleted(object sender, DragCompletedEventArgs e)
         {
             try
             {
-                RangeSlider1.MinRange = RangeSlider1.LowerValue;
+                //RangeSlider1.MinRange = RangeSlider1.LowerValue;
             }
             catch (Exception exception)
             {
                 MessageBox.Show(exception.Message + exception.StackTrace + exception.TargetSite);
             }
-            TestBlock.Text = "lower thumb drag completed";
+            //TestBlock.Text = "lower thumb drag completed";
         }
 
         private void RangeSlider_OnUpperThumbDragStarted(object sender, DragStartedEventArgs e)
         {
-            TestBlock.Text = "upper thumb drag started";
+            //TestBlock.Text = "upper thumb drag started";
         }
 
         private void RangeSlider_OnUpperThumbDragCompleted(object sender, DragCompletedEventArgs e)
         {
-            TestBlock.Text = "upper thumb drag completed";
+            //TestBlock.Text = "upper thumb drag completed";
         }
 
         private void RangeSlider_OnCentralThumbDragStarted(object sender, DragStartedEventArgs e)
         {
-            TestBlock.Text = "central thumb drag started";
+            //TestBlock.Text = "central thumb drag started";
         }
 
         private void RangeSlider_OnCentralThumbDragCompleted(object sender, DragCompletedEventArgs e)
         {
             
-            TestBlock.Text = "central thumb drag completed";
+            //TestBlock.Text = "central thumb drag completed";
         }
 
         private void RangeSlider_OnLowerThumbDragDelta(object sender, DragDeltaEventArgs e)
@@ -75,7 +75,7 @@ namespace MetroDemo.ExampleViews
 
         private void RangeSlider_OnCentralThumbDragDelta(object sender, DragDeltaEventArgs e)
         {
-            TestBlock.Text = "central thumb drag delta";
+            //TestBlock.Text = "central thumb drag delta";
         }
     }
 }
